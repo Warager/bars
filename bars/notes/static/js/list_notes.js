84c16d49e1afca922fb5fb8259e13efef2f5349c
Fomin Dmitry <fomin.dritmy@gmail.com>
2014-11-14 01:44:42 +0300

    common.js added
    getCookie replaced

--- bars/notes/static/js/list_notes.js
+++ bars/notes/static/js/list_notes.js
@@ -4,29 +4,6 @@
  * licensing@sencha.com
  * http://www.sencha.com/license
 */
-Ext.onReady(function() {
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
-                    }
-            }
-        }
-        return cookieValue;
-    }
-    Ext.Ajax.on('beforerequest', function(conn, options){
-        if (!(/^http:.*/.test(options.url) || /^https:.*/.test(options.url))) {
-            options.headers = options.headers || {};
-            options.headers["X-CSRFToken"] = getCookie('csrftoken');
-        }
-    }, this);
-});
 Ext.onReady(function () {
 
   Ext.QuickTips.init();
