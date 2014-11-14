d5e4e6c8690965462575e4f6b9339fb4420b4aa3
Fomin Dmitry <fomin.dritmy@gmail.com>
2014-11-14 09:50:37 +0300

    try to change csrf

--- bars/notes/static/js/common.js
+++ bars/notes/static/js/common.js
@@ -1,23 +1,52 @@
-Ext.onReady(function() {
-    function getCookie(name) {
+//Ext.onReady(function() {
+//    function getCookie(name) {
+//        var cookieValue = null;
+//        if (document.cookie && document.cookie != '') {
+//            var cookies = document.cookie.split(';');
+//            for (var i = 0; i < cookies.length; i++) {
+//                var cookie = jQuery.trim(cookies[i]);
+//                // Does this cookie string begin with the name we want?
+//                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+//                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//                        break;
+//                    }
+//            }
+//        }
+//        return cookieValue;
+//    }
+//    Ext.Ajax.on('beforerequest', function(conn, options){
+//        if (!(/^http:.*/.test(options.url) || /^https:.*/.test(options.url))) {
+//            options.headers = options.headers || {};
+//            options.headers["X-CSRFToken"] = getCookie('csrftoken');
+//        }
+//    }, this);
+//});
+
+
+
+$(function () {
+  $.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+      function getCookie(name) {
         var cookieValue = null;
         if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
-                    }
+          var cookies = document.cookie.split(';');
+          for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
             }
+          }
         }
         return cookieValue;
+      }
+
+      if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+        // Only send the token to relative URLs i.e. locally.
+        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+      }
     }
-    Ext.Ajax.on('beforerequest', function(conn, options){
-        if (!(/^http:.*/.test(options.url) || /^https:.*/.test(options.url))) {
-            options.headers = options.headers || {};
-            options.headers["X-CSRFToken"] = getCookie('csrftoken');
-        }
-    }, this);
+  })
 });
